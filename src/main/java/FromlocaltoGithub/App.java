9e04cd90aa6fb721ee22f0815eb7a82cdfdadb81
sasikalajaya <sasikalajaya@gmail.com>
2014-08-01 17:19:04 +0100

    Initial commit

---
+++ src/main/java/FromlocaltoGithub/App.java
@@ -0,0 +1,13 @@
+package FromlocaltoGithub;
+
+/**
+ * Hello world!
+ * this is the first commit and push to GitHub
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
